df6bf2cb0946aba7c5e0d5089b6e6aa0840c00e6
TomAugspurger <thomas-augspurger@uiowa.edu>
2013-01-03 20:16:20 -0600

    Refactored as class.

--- ngm_discrete.py
+++ ngm_discrete.py
@@ -16,42 +16,80 @@ from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 
-def ngm(alpha=.36, beta=.96, delta=.08, v_0=.01, k_n=100, k_l=.01,
-    k_u=30, epsilon=.00005, z=1, u=np.log, f=None, max_iter=1000):
+
+class NGM(object):
+    """Calculate a neoclassical growth model using value function iteration.
     """
-    """
-    k_v = np.arange(k_l, k_u, (k_u - k_l) / k_n)
-    k_grid = np.tile(k_v, (k_n, 1)).T
+    def __init__(self, alpha=.36, beta=.96, delta=.08, v_0=.01, k_n=100,
+        k_l=.01, k_u=30, epsilon=.00005, z=1, u=np.log, f=None,
+        max_iter=1000):
+        self.params = {'alpha': alpha,
+                    'beta': beta,
+                    'delta': delta,
+                    'v_0': v_0,
+                    'k_n': k_n,
+                    'k_l': k_l,
+                    'k_u': k_u,
+                    'epsilon': epsilon,
+                    'z': z,
+                    'u': u,
+                    'f': f,
+                    'max_iter': max_iter
+                    }
 
-    f = lambda k: k ** alpha
+    def ngm(self, **kwargs):
+        """
+        Call like vf, pr = NGM.ngm()
 
-    c = z * f(k_grid) + (1 - delta) * k_grid - k_grid.T
-    utility = u(c)
-    utility[c <= 0] = -100000
+        TODO: Takes args from self.params as a dict
+        TODO: check on v_0; right now assuming all 0.
+        """
+        k_l = self.params['k_l']
+        k_u = self.params['k_u']
+        k_n = self.params['k_n']
+        alpha = self.params['alpha']
+        z = self.params['z']
+        beta = self.params['beta']
+        delta = self.params['delta']
+        epsilon = self.params['epsilon']
+        u = self.params['u']
+        max_iter = self.params['max_iter']
 
-    e = 1
-    iteration = 0
-    value_function = np.zeros(k_n)
-    new_value_function = np.zeros(k_n)
-    policy_rule = np.zeros(k_n)
+        k_v = np.arange(k_l, k_u, (k_u - k_l) / k_n)
+        k_grid = np.tile(k_v, (k_n, 1)).T
 
-    while e > epsilon and iteration < max_iter:
-        for i in range(k_n):
-            temp = utility[i, :] + beta * value_function.T
-            ind = np.argmax(temp)
-            temp_vf = np.max(temp)
-            policy_rule[i] = k_v[ind]
-            new_value_function[i] = temp_vf
+        f = lambda k: k ** alpha
 
-            e = np.max(np.abs(value_function - new_value_function))
-            iteration += 1
-            value_function = new_value_function
+        c = z * f(k_grid) + (1 - delta) * k_grid - k_grid.T
+        utility = u(c)
+        utility[c <= 0] = -100000
 
-    return (value_function, policy_rule)
+        e = 1
+        iteration = 0
+        value_function = np.zeros(k_n)
+        new_value_function = np.zeros(k_n)
+        policy_rule = np.zeros(k_n)
 
-vf, pr = ngm()
-fig = plt.figure()
-ax1 = fig.add_subplot(2, 1, 1)
-ax1.plot(vf)
-ax2 = fig.add_subplot(2, 1, 2)
-ax2.plot(vf)
+        while e > epsilon and iteration < max_iter:
+            for i in range(k_n):
+                temp = utility[i, :] + beta * value_function.T
+                ind = np.argmax(temp)
+                temp_vf = np.max(temp)
+                policy_rule[i] = k_v[ind]
+                new_value_function[i] = temp_vf
+
+                e = np.max(np.abs(value_function - new_value_function))
+                iteration += 1
+                value_function = new_value_function
+
+        return (value_function, policy_rule)
+
+    def gen_plots(self, value_function, policy_rule):
+        """Get a plot of the value function & policy rules.
+        """
+        fig = plt.figure()
+        ax1 = fig.add_subplot(2, 1, 1)
+        ax1.plot(value_function)
+        ax2 = fig.add_subplot(2, 1, 2)
+        ax2.plot(policy_rule)
+        return fig
